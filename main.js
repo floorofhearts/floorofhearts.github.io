ef0dda616101c9ffd63124d9e7ea920fc5d0aa6e
Mikka <44730779+MikkaHYT@users.noreply.github.com>
2025-03-17 16:37:26 +0000

    add marquee, placeholder images and reviews

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+        section.classList.add("enable-animation");
+        const checkbox = section.querySelector("input");
+        });
+    });
+
+
