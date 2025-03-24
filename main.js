b621da3c5e43218a206ef44fae23cf3e4b743ea2
Mikka <44730779+MikkaHYT@users.noreply.github.com>
2025-03-24 17:10:32 +0000

    refactor navigation and search functionality; add LVT pages and improve layout

--- main.js
+++ main.js
@@ -2,8 +2,31 @@ document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll("section");
     sections.forEach((section) => {
         section.classList.add("enable-animation");
-        const checkbox = section.querySelector("input");
-        });
     });
 
+    // Add event listener for the search bar
+    const searchBar = document.querySelector(".search-bar input");
+    searchBar.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            const query = searchBar.value.trim(); // Get the search query
+            if (query) {
+                sessionStorage.setItem('recent-search-query', query);
+                window.location.href = `/search.html?search=${encodeURIComponent(query)}`;
+            } else if (sessionStorage.getItem('recent-search-query')) {
+                window.location.href = `/search.html?search=${encodeURIComponent(sessionStorage.getItem('recent-search-query'))}`;
+            }
+        }
+    });
 
+    // Add event listener for the search button
+    const searchButton = document.querySelector(".search-bar button");
+    searchButton.addEventListener("click", () => {
+        const query = searchBar.value.trim(); // Get the search query
+        if (query) {
+            sessionStorage.setItem('recent-search-query', query);
+            window.location.href = `/search.html?search=${encodeURIComponent(query)}`;
+        } else if (sessionStorage.getItem('recent-search-query')) {
+            window.location.href = `/search.html?search=${encodeURIComponent(sessionStorage.getItem('recent-search-query'))}`;
+        }
+    });
+});
